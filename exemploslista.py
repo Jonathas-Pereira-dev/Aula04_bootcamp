06b662a49844fd9fca080a7a2cfcd727aa62ce8b
Jonathas Pereira <pjonathas972@gmail.com>
2025-10-07 15:21:41 -0300

    Exemplos listas

---
+++ exemploslista.py
@@ -0,0 +1,12 @@
+produto: str = "Caneta"
+produto2: str = "Lápis"
+produto3: str = "Caderno"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto2)
+produtos.append(produto3)
+produtos.pop()  # Remove o último item da lista
+
+print(produtos)
